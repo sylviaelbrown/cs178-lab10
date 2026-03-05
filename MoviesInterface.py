101a321e35708517989a0fdc0b35a3872edb4672
Sylvia Brown <sylviabrown@Sylvias-MacBook-Air.local>
2026-03-05 17:41:32 -0600

    Challenge 1: update_rating with try/except

--- MoviesInterface.py
+++ MoviesInterface.py
@@ -59,14 +59,18 @@ def print_all_movies():
 
 
 
-
 def update_rating():
-    """
-    Prompt user for a Movie Title.
-    Prompt user for a rating (integer).
-    Append the rating to the movie's Ratings list in the database.
-    """
-    print("updating rating")
+    try: 
+        title = input("What is the movie title? ")
+        rating = int(input("What is the rating (integer): "))
+        table.update_item(
+            Key={"Title": title},
+            UpdateExpression="SET Ratings = list_append(Ratings, :r)",
+            ExpressionAttributeValues={':r': [rating]}
+        )
+        print("Rating added!")
+    except:
+        print("Error in updating movie rating")
 
 def delete_movie():
     """
